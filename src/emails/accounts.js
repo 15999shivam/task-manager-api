e65f5b73e66e0ee50cb07210f5d4365d1a3be001
Shivam <shivamgarg260@gmail.com>
2019-09-06 19:41:57 +0530

    added error handling on email sending

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -5,21 +5,29 @@ const sendgridAPIKEY = process.env.SENDGRID_API_KEY;
 sgMail.setApiKey(sendgridAPIKEY);
 
 const sendWelcomeEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "shivamgarg260@gmail.com",
-    subject: "thanks for joining in!",
-    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
-  });
+  try {
+    sgMail.send({
+      to: email,
+      from: "shivamgarg260@gmail.com",
+      subject: "thanks for joining in!",
+      text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 const sendCancelEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "shivamgarg260@gmail.com",
-    subject: "You Account have been Sucessfuly deleted",
-    text: `Dear, ${name} we are unhappy to see you go, We hope you will Come back one day, Good Bye`
-  });
+  try {
+    sgMail.send({
+      to: email,
+      from: "shivamgarg260@gmail.com",
+      subject: "You Account have been Sucessfuly deleted",
+      text: `Dear, ${name} we are unhappy to see you go, We hope you will Come back one day, Good Bye`
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
 module.exports = {
